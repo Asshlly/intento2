3c32fcbd836cb81bcaf0647fb3376a7fbd87f1fe
Asshlly <144860915+Asshlly@users.noreply.github.com>
2024-03-19 21:18:43 -0600

    Update vite.config.js

--- asshlly/vite.config.js
+++ asshlly/vite.config.js
@@ -5,7 +5,7 @@ import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import htmlPurge from 'vite-plugin-purgecss';
 export default defineConfig(
     {
-        base: '/',
+        base: '/intento2/',
         appType: 'mpa',
         build: {
             rollupOptions: {
